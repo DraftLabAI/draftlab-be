8bdeca465ead005395c7e72a8902e96d83fbdffe
changwoo <124979889+okchangwoo@users.noreply.github.com>
2025-12-19 00:03:37 +0900

    feat: api/ prefix 추가

--- src/main/java/com/example/draftlab/config/CorsConfig.java
+++ src/main/java/com/example/draftlab/config/CorsConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -19,6 +20,11 @@ public class CorsConfig {
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
+            }
+            
+            @Override
+            public void configurePathMatch(@NonNull PathMatchConfigurer configurer) {
+                configurer.addPathPrefix("/api", c -> c.isAnnotationPresent(org.springframework.web.bind.annotation.RestController.class));
             }
         };
     }
