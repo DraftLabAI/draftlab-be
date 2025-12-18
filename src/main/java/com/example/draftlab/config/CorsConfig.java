689ac1ec048450af0af1e4f38328c5280868f62a
changwoo <124979889+okchangwoo@users.noreply.github.com>
2025-12-19 01:16:32 +0900

    feat: swagger cors 설정

--- src/main/java/com/example/draftlab/config/CorsConfig.java
+++ src/main/java/com/example/draftlab/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")  // Vite 개발 서버
+                        .allowedOrigins("http://localhost:5173", "http://localhost:8080")  // Vite 개발 서버
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
